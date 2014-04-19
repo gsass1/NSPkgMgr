da167e37e2724a0c21992c1efaae10f6a9ad2648
Nuke928 <nuke928@gmail.com>
2014-04-19 16:27:29 +0200

    Allow file overwriting

--- NSPkgMgr/PackageManager.cs
+++ NSPkgMgr/PackageManager.cs
@@ -458,7 +458,7 @@ namespace NSPkgMgr
             foreach (FileInfo file in files)
             {
                 string temppath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(temppath, false);
+                file.CopyTo(temppath, true);
             }
 
             // If copying subdirectories, copy them and their contents to new location. 
